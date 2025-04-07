d2abd2f7913bfac20b51de560656ecef310d4b2c
Mazhar7867 <qureshimazher26@gmail.com>
2025-04-07 23:32:40 +0530

    third commit

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -2,7 +2,36 @@ import React from 'react'
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <div>
+      <div>
+        <h1 className='text-center text-5xl text-black'>About US</h1>
+
+        <div>
+          <img src="" alt="" />
+          <div>
+            <h1>Vision</h1>
+            <h2>Empowering Tomorrow Through Innovative Code
+At Black Grapes Softech, our vision is to lead the way in software development, using innovation and creativity to
+shape the future of technology. We envision a world where every software solution we create empowers individuals, businesses, and communities to thrive and excel.</h2>
+          </div>
+
+          <div>
+            <h1>Mssion</h1>
+            <h2>Crafting Excellence, One Line of Code at a Time
+Our mission is to provide exceptional software Development services tailored to our clients’ Unique needs. We are dedicated to delivering top notch, cutting-edge solutions while fostering a culture of collaboration, growth, and innovation.
+
+At Black Grapes Softech, we
+don’t just writecode; we build
+the foundation for a better,
+smarter, and more connected
+world
+
+
+Meet the founders</h2>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
