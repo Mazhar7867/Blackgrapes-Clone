e482ef7bdd1243971124fd011241f59056f09d02
Mazhar7867 <qureshimazher26@gmail.com>
2025-04-08 20:42:00 +0530

    Initial commit - blaBlackGrapes clone

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -13,10 +13,11 @@ const AboutUs = () => {
             <h2>Empowering Tomorrow Through Innovative Code
 At Black Grapes Softech, our vision is to lead the way in software development, using innovation and creativity to
 shape the future of technology. We envision a world where every software solution we create empowers individuals, businesses, and communities to thrive and excel.</h2>
+          
           </div>
 
           <div>
-            <h1>Mssion</h1>
+            <h1>Mssions</h1>
             <h2>Crafting Excellence, One Line of Code at a Time
 Our mission is to provide exceptional software Development services tailored to our clients’ Unique needs. We are dedicated to delivering top notch, cutting-edge solutions while fostering a culture of collaboration, growth, and innovation.
 
--- src/Components/Ecommerce.jsx
+++ src/Components/Ecommerce.jsx
@@ -15,7 +15,7 @@ const Ecommerce = () => {
      <h1 className='text-[#F86200] pt-5 pb-12'>WWW.nayapuranabazar.com</h1>
       </div>
       <p className='text-3xl leading-relaxed'>We are thrilled to announce the upcoming launch of Naya Purana Bazaar, your future favourite destination for online shopping. Our platform is under development, and we are working hard to bring you a seamless, user-friendly shopping experience with a wide range of “New And Used Products”. Stay tuned for more updates and be among the first to explore our unique offerings and exclusive deals. Thank you for your patience and support as we prepare to launch. Get ready to redefine your shopping experience with nayapuranabazaar.com!</p>
-       <div className='flex justify-between text-center text-4xl font-bold mt-6 '>
+       <div className='flex justify-between text-center text-4xl font-bold mt-6 mb-16 '>
        <div>
 
         <img className=' h-[800px] w-[900px] rounded-3xl mb-5' src={Eccomerce1} alt="image" />
