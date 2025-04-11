9eb7627e5ad38ef611de8b119f5a4fe0e91bfbbc
Mazhar7867 <qureshimazher26@gmail.com>
2025-04-11 11:37:03 +0530

    sixth commit

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import Footer from "../Footer"
 import about from "./Images/about.png"
+import founders from "./Images/founders.jpeg"
 const AboutUs = () => {
   return (
-    <div>
-        <div className='Ml[10%] mr[10%]'>
-        <h1 className='text-center text-5xl text-black'>About US</h1>
+    <>
+        <div className='Ml-[10%] mr-[10%]'>
+        <h1 className='text-center text-5xl font-bold text-black mt-36'>About US</h1>
+             <div className='flex justify-around h-full mt-36 mb-36'>
 
-             <div className='flex justify-between'>
-          <img className='h-[300px] w-[300px]' src={about} alt="" />
-          <div>
-            <h1>Vision</h1>
-            <h2>Empowering Tomorrow Through Innovative Code
-At Black Grapes Softech, our vision is to lead the way in software development, using innovation and creativity to
+          <img className='h-[600px] w-[600px]' src={about} alt="" />
+          <div className='w-[500px]'>
+            <h1 className='text-center text-4xl font-bold mb-16' >Vision</h1>
+            <h2 className='text-2xl leading-relaxed'> <b> Empowering Tomorrow Through Innovative Code </b> <br />
+At Black Grapes Softech,  our vision is to lead the way in software development, using innovation and creativity to
 shape the future of technology. We envision a world where every software solution we create empowers individuals, businesses, and communities to thrive and excel.</h2>
           
           </div>
 
-          <div>
-            <h1>Mssions</h1>
-            <h2>Crafting Excellence, One Line of Code at a Time
+          <div className='w-[500px]'>
+            <h1 className='text-center text-4xl font-bold mb-16'>Mssions</h1>
+            <h2 className='text-2xl leading-relaxed'><b> Crafting Excellence, One Line of Code at a Time </b> <br />
 Our mission is to provide exceptional software Development services tailored to our clients’ Unique needs. We are dedicated to delivering top notch, cutting-edge solutions while fostering a culture of collaboration, growth, and innovation.
-
+<br />
+<br />
+<br />
 At Black Grapes Softech, we
 don’t just writecode; we build
 the foundation for a better,
@@ -33,8 +36,14 @@ Meet the founders</h2>
           </div>
         </div>
       </div>
+      <div className='flex justify-between text-2xl'>
+        <img src={founders} alt="" />
+        <div>
+          <h1 className='text-5xl' >Meet the founders</h1>
+        </div>
+      </div>
       <Footer/>
-    </div>
+    </>
   )
 }
 
--- src/Components/OurClients.jsx
+++ src/Components/OurClients.jsx
@@ -5,7 +5,7 @@ const OurClients = () => {
     <div>
     <div className='ml-[10%] mr-[10%]'>
 
-      <h1 className='text-center text-5xl mt-28 mb-11 '>OurClients</h1>
+      <h1 className='text-center text-5xl mt-28 mb-11'>OurClients</h1>
       <p className='text-xl'>At Black Grapes Softech, we take pride in our diverse and dynamic client portfolio. Our clients span various industries including finance, healthcare, e-commerce, education, and technology. We partner with both startups and established enterprises to deliver innovative software solutions tailored to their unique needs. Our commitment to excellence and customer satisfaction has earned us long-standing relationships with clients globally. Whether it’s developing custom applications, enhancing cybersecurity, or implementing advanced data analytics, our clients trust us to drive their digital transformation and support their growth. Join our ever-growing family of satisfied clients and experience the Black Grapes Softech difference.</p>
 
       </div>
